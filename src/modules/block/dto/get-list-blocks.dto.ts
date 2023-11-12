ce12e2c575a06c6af5d9ab34a3646a288c682253
Tuan Vu <vuanhtuan1407@gmail.com>
2023-11-12 16:50:13 +0700

    Update: DTO index, count required

--- src/modules/block/dto/get-list-blocks.dto.ts
+++ src/modules/block/dto/get-list-blocks.dto.ts
@@ -1,16 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsNumber, IsOptional } from 'class-validator';
+import { IsNumber } from 'class-validator';
 
 export class GetListBlocks {
-    @ApiProperty({ type: 'string', required: false, example: '0' })
-    @IsOptional()
+    @ApiProperty({ type: 'string', required: true, example: '0' })
     @Type(() => Number)
     @IsNumber()
     index: number;
 
-    @ApiProperty({ type: 'string', required: false, example: '5' })
-    @IsOptional()
+    @ApiProperty({ type: 'string', required: true, example: '5' })
     @Type(() => Number)
     @IsNumber()
     count: number;
