bad788e145241ca8f046ca88dd1d124e49f208cd
Huyen0402 <huyentran1232002@gmail.com>
2023-11-13 23:50:00 +0700

    Dev token

--- src/modules/settings/dto/set-devtoken.dto.ts
+++ src/modules/settings/dto/set-devtoken.dto.ts
@@ -1,13 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
 import { DevTokenType } from 'src/constants/dev-token-type.enum';
 
 export class SetDevtokenDto {
     @ApiProperty({
+        type: 'string',
         example: '1',
     })
     @IsNotEmpty()
-    @IsString()
+    @Type(() => Number)
+    @IsEnum(DevTokenType)
     devtype: DevTokenType;
 
     @ApiProperty({
--- src/modules/settings/settings.service.ts
+++ src/modules/settings/settings.service.ts
@@ -9,23 +9,23 @@ import { SetDevtokenDto } from './dto/set-devtoken.dto';
 export class SettingsService {
     constructor(
         @InjectRepository(DevToken)
-        private devTokenRepository: Repository<DevToken>,
+        private devTokenRepo: Repository<DevToken>,
     ) {}
 
     async setDevtoken(user: User, body: SetDevtokenDto) {
-        const existingDevToken = await this.devTokenRepository.findOne({ where: { userId: user.id } });
+        const existingDevToken = await this.devTokenRepo.findOne({ where: { userId: user.id } });
 
         if (existingDevToken) {
             existingDevToken.type = body.devtype;
             existingDevToken.token = body.devtoken;
-            await this.devTokenRepository.save(existingDevToken);
+            await this.devTokenRepo.save(existingDevToken);
         } else {
             const newDevToken = new DevToken({
                 userId: user.id,
                 type: body.devtype,
                 token: body.devtoken,
             });
-            await this.devTokenRepository.save(newDevToken);
+            await this.devTokenRepo.save(newDevToken);
         }
 
         return {};
